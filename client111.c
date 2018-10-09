379c0496f9ec360247058bd4125e883684c9182c
GodJarvan <823818335@qq.com>
2018-10-09 09:30:02 +0800

    新功能的实现
    1.更好的来储存用户的名字，之前是因为没有意识到recv的阻塞和非阻塞问题
    2.能简单的由一方与指定的一方进行对话，但是另一方是否接受的这一信息返回未完成
    3.在用户数组里面添加了新成员，用来标记与谁对话，正在对话时有谁想要与其对话等等，但还未实现其功能

--- client111.c
+++ client111.c
@@ -1,52 +1,305 @@
-#include<stdio.h>                                                               
-#include<unistd.h>
-#include<sys/socket.h>
-#include<sys/types.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h>
+#include <strings.h>
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <netdb.h>     
 #include<string.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
 
-#define SERVER_PORT 9999
-#define SERVER_IP "192.168.43.121"
 
-int main(int argc, char *argv[])
+#define PORT 1234   
+#define MAXDATASIZE 100   
+
+char sendbuf[1024];
+char sendbuf_1[1024];
+char _sendbuf[1024];
+char _sendbuf_1[1024];
+char _recvbuf[1024];
+
+char recvbuf[1024];
+char name[100];   //自己的名字
+char Chat_Name[6]; //聊天人的名字
+int fd;
+int _fd;
+
+void pthread_recv_group(void* ptr)
 {
-    if(argc != 2)
-    {   printf("Usage: client IP \n");
-        return 1;
-    }
-    char *str = argv[1];
-    char buf[1024];
-    memset(buf, '\0', sizeof(buf));
 
-    struct sockaddr_in server_sock;
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    bzero(&server_sock, sizeof(server_sock));
-    server_sock.sin_family = AF_INET;
-    inet_pton(AF_INET, SERVER_IP, &server_sock.sin_addr);
-    server_sock.sin_port = htons(SERVER_PORT);
-    int ret = connect(sock, (struct sockaddr*)&server_sock, sizeof(server_sock));
-    if(ret < 0)
-    {
-        printf("perror");
-        return 1;
-    }
-    printf("connect success....\n");
-    while(1)
-    {
-        printf("client#:");
-        fgets(buf, sizeof(buf), stdin);
-        buf[strlen(buf)-1] = '\0';
-        write(sock, buf, sizeof(buf));
-        if(strncasecmp(buf, "quit", 4) == 0)
+        while(1)
         {
-            perror("perror");
-            break;
+            if ((recv(fd,recvbuf,MAXDATASIZE,0)) == -1){ 
+                printf("recv() error\n"); 
+                exit(1); 
+            } 
+
+            printf("%s",recvbuf);
+            memset(recvbuf,0,sizeof(recvbuf));
         }
-        printf("please wait...\n");
-        read(sock, buf, sizeof(buf));
-        printf("server$: %s\n",buf);                                            
-    }
-    close(sock);
-    return 0;
+}
+
+
+int Choose_Box()
+{
+	int num;
+	printf("请选择你想使用的聊天气泡的序号:\n");
+	printf("< 1 >\n");
+	printf("* 2 *\n");
+        printf("& 3 &\n");
+        printf("# 4 #\n");
+        printf("{ 5 }\n");
+        printf("@ 6 @\n");
+        printf("% 7 %\n");
+        printf("8:不使用聊天气泡\n");
+        printf("若选择序号错误，则默认不使用气泡。\n");
+	scanf("%d", &num);
+        return num;
+}
+
+void Box(int n, int m)
+{
+        int i, j, k, h;
+    	switch(n)
+	{
+		case 1:
+                    
+                    for(i=0; i<m; i++)
+                    {
+                        sendbuf_1[i]=' ';
+                    }
+                    sendbuf_1[m]='<';
+                    strncat(sendbuf_1,sendbuf,strlen(sendbuf));
+                    if (sendbuf_1[strlen(m+sendbuf)] == '\n')
+                    {
+                        sendbuf_1[strlen(m+sendbuf)] = '\0'; //因为是用的fgets来读取的要发送的信息，但是末尾是自动换行的，所以为了避免格式的错误，应该将末尾的'\n'换成'\0'。
+                    }   
+
+                    sendbuf_1[strlen(sendbuf)+m]='>';
+                    sendbuf_1[strlen(sendbuf)+1+m]='\n';
+                break;
+                case 2:
+                    
+                    for(j=0; j<m; j++)
+                    {
+                        sendbuf_1[j]=' ';
+                    }
+                    sendbuf_1[m]='*';
+                    strncat(sendbuf_1,sendbuf,strlen(sendbuf));
+                    if (sendbuf_1[strlen(m+sendbuf)] == '\n')
+                    {
+                        sendbuf_1[strlen(m+sendbuf)] = '\0';
+                    }   
+
+                    sendbuf_1[strlen(sendbuf)+m]='*';
+                    sendbuf_1[strlen(sendbuf)+1+m]='\n';
+                break;
+                case 3:
+                    
+                    for(k=0; k<m; k++)
+                    {
+                        sendbuf_1[k]=' ';
+                    }
+                    sendbuf_1[m]='&';
+                    strncat(sendbuf_1,sendbuf,strlen(sendbuf));
+                    if (sendbuf_1[strlen(m+sendbuf)] == '\n')
+                    {
+                        sendbuf_1[strlen(m+sendbuf)] = '\0';
+                    }   
+
+                    sendbuf_1[strlen(sendbuf)+m]='&';
+                    sendbuf_1[strlen(sendbuf)+1+m]='\n';
+                break;
+            
+                case 4:
+                    
+                    for(k=0; k<m; k++)
+                    {
+                        sendbuf_1[k]=' ';
+                    }
+                    sendbuf_1[m]='#';
+                    strncat(sendbuf_1,sendbuf,strlen(sendbuf));
+                    if (sendbuf_1[strlen(m+sendbuf)] == '\n')
+                    {
+                        sendbuf_1[strlen(m+sendbuf)] = '\0';
+                    }   
+
+                    sendbuf_1[strlen(sendbuf)+m]='#';
+                    sendbuf_1[strlen(sendbuf)+1+m]='\n';
+                break;
+             
+                case 5:
+                    
+                    for(k=0; k<m; k++)
+                    {
+                        sendbuf_1[k]=' ';
+                    }
+                    sendbuf_1[m]='{';
+                    strncat(sendbuf_1,sendbuf,strlen(sendbuf));
+                    if (sendbuf_1[strlen(m+sendbuf)] == '\n')
+                    {
+                        sendbuf_1[strlen(m+sendbuf)] = '\0';
+                    }   
+
+                    sendbuf_1[strlen(sendbuf)+m]='}';
+                    sendbuf_1[strlen(sendbuf)+1+m]='\n';
+                break;
+                case 6:
+                    
+                    for(k=0; k<m; k++)
+                    {
+                        sendbuf_1[k]=' ';
+                    }
+                    sendbuf_1[m]='@';
+                    strncat(sendbuf_1,sendbuf,strlen(sendbuf));
+                    if (sendbuf_1[strlen(m+sendbuf)] == '\n')
+                    {
+                        sendbuf_1[strlen(m+sendbuf)] = '\0';
+                    }   
+
+                    sendbuf_1[strlen(sendbuf)+m]='@';
+                    sendbuf_1[strlen(sendbuf)+1+m]='\n';
+                break;
+                case 7:
+                    
+                    for(k=0; k<m; k++)
+                    {
+                        sendbuf_1[k]=' ';
+                    }
+                    sendbuf_1[m]='%';
+                    strncat(sendbuf_1,sendbuf,strlen(sendbuf));
+                    if (sendbuf_1[strlen(m+sendbuf)] == '\n')
+                    {
+                        sendbuf_1[strlen(m+sendbuf)] = '\0';
+                    }   
+
+                    sendbuf_1[strlen(sendbuf)+m]='%';
+                    sendbuf_1[strlen(sendbuf)+1+m]='\n';
+                break;
+                case 8:
+                    for(h=0; h<m; h++)
+                    {
+                        sendbuf_1[h]=' ';
+                    }
+                    strncat(sendbuf_1,sendbuf,strlen(sendbuf));
+                break;
+                default:
+                    for(h=0; h<m; h++)
+                    {
+                        sendbuf_1[h]=' ';
+                    }
+                    strncat(sendbuf_1,sendbuf,strlen(sendbuf));
+                    
+                break;
+        }
+  
+}
+
+int Message(char friend[])
+{
+        int n;
+        printf("%s wanna chat with you. If you accept please press 1, refuse please  press 0.\n", friend);
+        scanf("%d", &n);
+        return n;
+}
+
+int main(int argc, char *argv[]) 
+{
+	int  numbytes;   
+	char buf[MAXDATASIZE];   
+    	struct hostent *he;       
+    	struct sockaddr_in server;  
+
+
+    	if (argc !=2) {         
+		printf("Usage: %s <IP Address>\n",argv[0]); 
+    		exit(1); 
+    	} 
+
+
+    	if ((he=gethostbyname(argv[1]))==NULL){  
+    		printf("gethostbyname() error\n"); 
+    		exit(1); 
+    	} 
+
+    	if ((fd=socket(AF_INET, SOCK_STREAM, 0))==-1){ 
+    		printf("socket() error\n"); 
+    		exit(1); 
+    	} 
+
+    	bzero(&server,sizeof(server));
+
+    	server.sin_family = AF_INET; 
+    	server.sin_port = htons(PORT); 
+    	server.sin_addr = *((struct in_addr *)he->h_addr);  
+    	if(connect(fd, (struct sockaddr *)&server,sizeof(struct sockaddr))==-1){  
+    		printf("connect() error\n"); 
+    		exit(1); 
+    	} 
+
+    	printf("connect success\n");
+        
+        recv(fd,recvbuf,MAXDATASIZE,0);
+        printf("%s\n", recvbuf);
+        memset(recvbuf,0,sizeof(recvbuf));
+        
+    	char str[]=" 已进入聊天室\n";
+    	printf("请输入用户名（最多六个字母）：");
+    	fgets(name,sizeof(name),stdin);
+      
+        printf("提醒：退出聊天时请输入“quit若失败请多次尝试输入\n");
+        printf("\n");
+        
+        char answer[1];
+        
+        printf("选择群聊请按1, 私聊请按2\n");
+        
+        scanf("%s", &answer);
+        send(fd, answer, 1, 0); 
+        
+        if(strcmp(answer, "2")==0)
+        {
+            memset(Chat_Name, 0, sizeof(Chat_Name));
+    
+            printf("请输入你想要聊天的人的名字并在后面加‘-’(如：张三- 和 李四-): \n");
+            scanf("%s", &Chat_Name);
+            send(fd, Chat_Name, 6, 0);
+        }
+        
+        
+        send(fd,name,(strlen(name)-1),0);
+        
+        send(fd,name,(strlen(name)-1),0);
+        send(fd,str,(strlen(str)),0);
+        
+        
+            pthread_t tid;
+            pthread_create(&tid,NULL,(void*)pthread_recv_group,&fd);
+    
+            int box=8;
+//          box=Choose_Box();
+
+            while(1)
+            {         
+                    memset(sendbuf,0,sizeof(sendbuf));
+                    memset(sendbuf_1,0,sizeof(sendbuf_1));
+                    fgets(sendbuf,sizeof(sendbuf),stdin);
+                
+                    Box(box, strlen(name)-1);
+
+                    if(strcmp(sendbuf,"exit\n")==0){
+                            memset(sendbuf,0,sizeof(sendbuf));
+                            printf("您已退出群聊\n");
+                            send(fd,sendbuf,(strlen(sendbuf)),0);
+                            break;
+                    }
+                    if(strlen(sendbuf)>1)   //防止用户在聊天时没有打字也按空格就发出去的情况
+                    {
+                        send(fd,name,(strlen(name)-1),0);
+                        send(fd,":\n",2,0);
+                        send(fd,sendbuf_1,(strlen(sendbuf_1)),0);  //sendbuf_1是添加过气泡的消息
+                    }
+
+            }
+            close(fd);
 }
--- server111.c
+++ server111.c
@@ -1,89 +1,330 @@
-#include<stdio.h>
+#include <stdio.h>          
+#include <unistd.h>          
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <arpa/inet.h>
+#include<pthread.h> 
 #include<stdlib.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<unistd.h>
-#include<string.h>
-#include<sys/types.h>
+#include <string.h>
+#include <ctype.h>
+
+#define PORT 1234    
+#define BACKLOG 1 
+#define Max 10
+#define MAXSIZE 2048
+
+//连接后记录confd数组
+char information[100];  //提示有人进入聊天室
+int confd[50] = {};
+char Nameset[200];
+char nameset[200]={};
+//设置连接人数
+int count = 0;
+//每个人的信息结构
+typedef struct user{
+  char name [100];
+  int flag;       //是否在线 
+  int object;     //正在私聊的对象的fd 没有的话为-1
+  int status;    //是否忙碌 没有私聊的话为0, 忙碌的话为1
+  int fans;       //想要跟自己聊天的人， 没有时为0, 可以累加
+  int number;
+}user;
+
+//结构体数组
+user info [50]={}; 
 
 
-#define _PORT_ 9999
-#define _MAXLOG_ 5
+/*定义全局变量*/
+int fdt[Max]={0};
+char mes[1024];
+char _mes[1024];
+int number=0;
+
+char buf_snd[50];
+char _buf_snd[50];
 
 
-int main()
+int SendToClient(int fd,char* buf,int Size, int index)
 {
+    int i;
+    int k;
+    memset(Nameset,0,200);
+    
+    for(k=0;k<=count;k++){
+        if(info[k].flag==0){
+         
+            continue;
+        }
+        strcat(Nameset,info[k].name);
+    }
+    
+    
+    memset(nameset,0,200);
+    
+    sprintf(nameset,"(当前在线的有%s)\n",Nameset);
+    
+    
+    for(i=0;i<Max;i++)
+    {
+        //printf("fdt[%d]=%d\n",i,fdt[i]);   
+        
+        if((fdt[i]!=0)&&(fdt[i]!=fd))
+        {
+        
+            send(fdt[i],buf,Size,0); 
+            //printf("send message to Name:%s fdth:%d\n",info[i].name, fdt[i]);
+            
+        }
+    }
+    return 0;
+}
 
-	
-	int sock = socket(AF_INET, SOCK_STREAM, 0);
+int Search_Name(char a[8])
+{
+        int i;
+        for(i=0; i<50; i++)
+        {
+            printf("info[i].name : %s\n", info[i]);
+            if(strcmp(a, info[i].name)==0)
+            {
+                return i;
+            }
+        }
+        return -1;
+}
 
-	int opt = 1;
-	setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+void *pthread_service(void* sfd)
+{    
+    
+    int fd=*(int *)sfd;  
+    int index;
+    
+    send(fd,nameset,sizeof(nameset),0);
+    
+    
+    int n;
+    for(n=0; n<50; n++)
+    {
+        if(confd[n]==fd)
+        {
+            index=n;
+            break;
+        }
+    }
+    
+    info[index].number=index;
+    info[index].flag=1;
+    char Name[20];
+    
+    char b[1];
+    recv(confd[index], b, 1, 0);
+    
+    
+    if(strcmp(b, "1")==0)    //选择群聊
+    {
+    
+        recv(confd[index],Name,6,0);
+        strcpy(info[index].name,Name);
+        char* jiange="-";
+        strcat(info[index].name,jiange);
 
+    
+        int k=0;
+        int flag;
+    
+        while(1)
+        {
+            int numbytes;
+            int i;
+            numbytes=recv(fd,mes,MAXSIZE,0);
 
-	struct sockaddr_in server_socket;
-	struct sockaddr_in client_socket;
+/*        if(k==0)
+        {
+                int m=0;
+                int n=0;
+                while((mes[m]&&mes[m]!=' ') && isascii(mes[m]))
+                {
+                    m++;
+                }
+                flag=m;
+                strncpy(Name, mes, m);
+                Name[m]='\0';
 
-	bzero(&server_socket, sizeof(server_socket));
-
-	server_socket.sin_family = AF_INET;
-	server_socket.sin_addr.s_addr = htonl(INADDR_ANY);
-	server_socket.sin_port = htons(_PORT_);
-
-	bind(sock, (struct sockaddr*)&server_socket, sizeof(struct sockaddr_in));
-
-	listen(sock, _MAXLOG_);
-
-	printf("wait accept..\n");
-
-	while(1)
-	{
-	socklen_t len =0;
-	int client_sock = accept(sock, (struct sockaddr*)&client_sock, &len);
-
-	char buf_ip[INET_ADDRSTRLEN];
-	memset(buf_ip, '\0', sizeof(buf_ip));
-	
-	inet_ntop(AF_INET, &client_socket.sin_addr, buf_ip, sizeof(buf_ip));
-	printf("get connect, ip is : %s port is  %d\n",buf_ip, ntohs(client_socket.sin_port));
-
+                        
+                strncpy(info[index].name , Name, m);
+                char* jiange="--";
+                strcat(info[index].name,jiange);
+                info[index].name[m+1]='\0';
+        }
 
         
-        
-	char othername[10];
-	memset(othername, '\0', sizeof(othername));
-	char name[10];
-	memset(name, '\0', sizeof(name));
-	printf("please input your name\n");
-	fgets(name, sizeof(name), stdin);
+*/        
+            char a[4];
+
+            memset(buf_snd,0,50);
         
         
-	send(client_sock, name, sizeof(name), 0);
-        recv(client_sock, othername, sizeof(othername), 0);
-	othername[strlen(othername)-1]='\0';
+            if((0 == strcmp("quit",strncpy(a, mes+flag+2, 4)))||(0 == strcmp("quit",strncpy(a, mes, 4)))||(0 == strcmp("quit",strncpy(a, mes+flag+3, 4))) || (0 == strcmp("quit",strncpy(a+1, mes, 4))))  //因为"mes"有时候会带上名字，有时候没有带名字
+            {
+  
+                sprintf(buf_snd,"%s已经退出聊天室\n",info[index].name);
+                int i;
+                for(i = 0;i <= count;i++)
+                {
+                    if(i == index || 0 == confd[i])
+                    {
+                        continue;
+                    }
+                    send(confd[i],buf_snd,strlen(buf_snd),0);
+                }
+                confd[index] = -1;
+                info[index].flag=0;
+                info[index].number=1024;
+                close(fd);
+                break;
+            }
+        
+        
+            if(numbytes<=0){
+                for(i=0;i<Max;i++){
+                    if(fd==fdt[i]){
+                        fdt[i]=0;               
+                    }
+                }
+                break;
+            }
 
-		while(1)
-		{
+            SendToClient(fd,mes,numbytes, index);
+            k++;
+        }
+        close(fd);
+        
+    }
+    
+    else if(strcmp(b, "2")==0)  //选择私聊
+    {
+      
+            char* jiange="-";
+            char chat_name[8];
+            recv(confd[index],chat_name,6,0);
+            strcat(chat_name,jiange);
+//            printf("聊天对象: %s\n", chat_name);
+            
+            recv(confd[index],Name,6,0);
+//            printf("自己的名字：%s\n", Name);
+            
+            strcpy(info[index].name,Name);
+            strcat(info[index].name,jiange);
+            
+            strcat(Nameset, Name);
+            
+            
+            
+            int c=Search_Name(chat_name);
+            if(c==-1) printf("对不起，没有这个人。\n");
+            else printf("正在请求与其对话\n");
+            
+            send(confd[c], Name, strlen(Name), 0);
+            send(confd[c], "请求与你私聊， 同意请选择1, 拒绝请选择2 (ATTENTION)\n",100, 0);
+            //发送给聊天对象
+/*          char answer[1];
+            recv(confd[c],answer,1,0);
+            printf("Answer: %s\n", answer);
+ */           
+            memset(Nameset,0,200);
+    }
+        
+}
+            
+            
+int  main()  
+{ 
+    int listenfd, connectfd;    
+    struct sockaddr_in server; 
+    struct sockaddr_in client[50];      
+    int sin_size; 
+    sin_size=sizeof(client[0]); 
+    int fd;
 
 
-			
-			
-    			char buf[20];
-			memset(buf, '\0', sizeof(buf));
-			read(client_sock, buf, sizeof(buf));
-                        printf("%s:%s\n", othername, buf);
-			
-			//printf("%s:", name);
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
+    {   
+        perror("Creating socket failed.");
+        exit(1);
+    }
 
-			memset(buf, '\0', sizeof(buf));
-			fgets(buf, sizeof(buf), stdin);
-			buf[strlen(buf) - 1]='\0';
-			write(client_sock, buf, strlen(buf)+1);
-			printf("please wait...\n");
-		}
-	}
-	close(sock);
-	return 0;
-	
 
+    int opt = SO_REUSEADDR;
+    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    bzero(&server,sizeof(server));  
+
+
+    server.sin_family=AF_INET; 
+    server.sin_port=htons(PORT); 
+    server.sin_addr.s_addr = htonl (INADDR_ANY); 
+
+
+    if (bind(listenfd, (struct sockaddr *)&server, sizeof(struct sockaddr)) == -1) { 
+    perror("Bind error.");
+    exit(1); 
+    }   
+
+
+    if(listen(listenfd,BACKLOG) == -1){  
+    perror("listen() error\n"); 
+    exit(1); 
+    } 
+    printf("Waiting for client....\n");
+
+    int k;
+    for(k=0;k<50;k++)
+    {
+      info[k].flag=0;
+      info[k].status=0;
+      info[k].object=-1;
+      info[k].fans=0;
+      memset(info[k].name,0,20);
+      info[k].number=1024;
+    }
+    
+    while(1)
+    {
+        
+        if ((fd = accept(listenfd,(struct sockaddr*)&client[count],&sin_size))==-1) {
+            perror("accept() error\n"); 
+            exit(1); 
+        }
+        
+        
+        if(number>=Max){
+            printf("no more client is allowed\n");
+            close(fd);
+        }
+
+        
+        int i;
+        for(i=0;i<Max;i++){
+            if(fdt[i]==0){
+                fdt[i]=fd;
+                break;
+            }
+
+        }
+        
+        confd[count]=fd;
+        ++count;
+    
+        
+        pthread_t tid;
+        
+        pthread_create(&tid,NULL,(void*)pthread_service,&fd);
+        
+        
+        number=number+1;
+        
+    }
+
+    close(listenfd);            
 }
